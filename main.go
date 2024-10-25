5524bebb499e657af38712dc1b008405ca828ed8
Leonardo Castro <ldcmleo19@gmail.com>
2024-10-24 23:34:17 -0500

    updates

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+func testHandler(myVar string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(myVar))
+	}
+}
+
 func main() {
-	fmt.Println("hola mundo")
+	mux := http.NewServeMux()
+
+	th := testHandler("hola mundo")
+	mux.Handle("/test", th)
+
+	log.Print("Listening... ")
+	http.ListenAndServe(":3000", mux)
 }
